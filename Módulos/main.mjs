bf8adbbd492a1a5363df546d791ef4eb5e886c93
EduardoESP <eduardopaulosantana@gmail.com>
2022-05-20 14:57:23 -0300

    Realizado pratica de programação em modulos

---
+++ Módulos/main.mjs
@@ -0,0 +1,7 @@
+import{
+    mostraIdade, mostraCidade, mostraHobby
+} from "./funcoes.mjs";
+
+console.log(mostraIdade('Eduardo', 34));
+console.log(mostraCidade('Eduardo', 'Rio de Janeiro'));
+console.log(mostraHobby('Eduardo', 'Séries'));
